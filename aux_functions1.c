8ffa4f02c98f1d60950777a315d1cbea95f44ee0
EricAltez <ericaltez1111@gmail.com>
2021-11-30 14:54:17 -0800

    add token function

--- aux_functions1.c
+++ aux_functions1.c
@@ -1,4 +1,37 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *token - divide given string in tokens
+ *@line: given string
+ *Return: array of strings
+ */
+char **token(char *line)
+{
+	int i = 0;
+	char *tok = NULL;
+	char **tokarray = NULL;
+
+	tokarray = malloc(1024);
+	if (!tokarray)
+	{
+		free(tokarray);
+		exit(3);
+	}
+	tok = strtok(line, " ");
+	while (tok != NULL)
+	{
+		tokarray[i] = tok;
+		tok = strtok(NULL, " ");
+		i++;
+	}
+	tokarray[i] = NULL;
+	for (i = 0; tokarray[i] != NULL; i++)
+	{
+		printf("%s\n", tokarray[i]);
+	}
+return (tokarray);
+}
 
 /**
  *prompt - dislplay a promp in the therminal
@@ -10,8 +43,8 @@ int prompt(void)
 {
 int first_time = 1;
 char *clear_string = "'\0'"; /** ver si funciona*/
-
-if (isatty(STDIN_FILENO) == 1) /**isatty() returns 1 if fd is an open file descriptor referring to a terminal;*/
+/**isatty() returns 1 if fd is an open file descriptor referring to a termin*/
+if (isatty(STDIN_FILENO) == 1)
 {
 	if (first_time == 1)
 	{
